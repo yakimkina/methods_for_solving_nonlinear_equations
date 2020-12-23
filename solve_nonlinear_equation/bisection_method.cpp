6803a153dac6b5c2b8ed880627bec3e3652e6c42
Polina Yakimkina <bvt3rfl1@gmail.com>
2020-12-23 17:25:18 +0300

    модификация метода Ньютона, версия без зацикливания

--- solve_nonlinear_equation/bisection_method.cpp
+++ solve_nonlinear_equation/bisection_method.cpp
@@ -2,37 +2,54 @@
 
 point	bisection_method(VALUE_TYPE (*f)(VALUE_TYPE), point a, point b)
 {
-	point	root;
-	root.x = (a.x + b.x) / 2;
-   	root.y = f(root.x);
+	int k = 0; // количество итераций
 
-   	VALUE_TYPE	x0 = root.x;
-   	int k = 1; // количество итераций
-	while (b.x - a.x > 2 * EPSILON)
+	point	root; // искомый корень уравнения
+	VALUE_TYPE	x0; // сохраняем начальное приближение(для вывода на экран)
+
+
+	/* случай, когда ноль лежит в самой правой границе отрезка (в дальнейшем
+ 	* алгоритм построен так, что значения в правых точках будут смотреться
+ 	* на этапе подсчета root.y, то есть на итерацию раньше, и смысла проверять
+ 	* значени b на каждой итерации нет) */
+	if (abs(b.y) < EPSILON)
 	{
-		if (a.y * root.y < 0)
-		{
-			b.x = root.x;
-			b.y = root.y;
-		}
-		else if (a.y == 0)
-		{
-			root.x = a.x;
-			root.y = a.y;
-			break;
-		}
-		else if (root.y == 0)
-			break;
-		else
-		{
-			a.x = root.x;
-			a.y = root.y;
-		}
-
+		root.x = b.x;
+		root.y = b.y;
+		x0 = root.x;
+	}
+	else
+	{
 		root.x = (a.x + b.x) / 2;
-		root.y = f(root.x);
+   		root.y = f(root.x);
+		x0 = root.x;
 
-		k += 1;
+		while (b.x - a.x > 2 * EPSILON)
+		{
+			if (abs(a.y) < EPSILON)	// левая граница отрезка совпала с нулем функции
+			{
+				root.x = a.x;
+				root.y = a.y;
+				break;
+			}
+			else if (abs(root.y) < EPSILON)	// правая граница отрезка совпала с нулем функции
+				break;
+			else if (a.y * root.y < 0)
+			{
+				b.x = root.x;
+				b.y = root.y;
+			}
+			else
+			{
+				a.x = root.x;
+				a.y = root.y;
+			}
+
+			root.x = (a.x + b.x) / 2;
+			root.y = f(root.x);
+
+			k += 1;
+		}
 	}
 
 	cout.precision(PREC);
